f69c56a1d7dcd364f726c79c5a1c9fc4c7650732
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-06-14 10:25:20 -0300

    update

--- src/main/java/com/semanaArte/controller/VisitanteController.java
+++ src/main/java/com/semanaArte/controller/VisitanteController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.semanaArte.model.Visitante;
 import com.semanaArte.service.VisitanteService;
 
+@RestController
+@RequestMapping("/visitante")
 public class VisitanteController {
 	
     @Autowired
