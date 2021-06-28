447b37f69dbdadbe978c3fc8116591869b7110e1
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-06-28 10:15:00 -0300

    Request value

--- src/main/java/com/semanaArte/controller/VisitanteController.java
+++ src/main/java/com/semanaArte/controller/VisitanteController.java
@@ -9,7 +9,7 @@ import com.semanaArte.model.Visitante;
 import com.semanaArte.service.VisitanteServiceImpl;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = {"/", "/pop-art", "/urban-art"})
 @CrossOrigin(origins = "https://sa-semana-da-arte.vercel.app", maxAge = 3600)
 public class VisitanteController {
 	
