e5f3b21b539269150d554f70784b4072bd629845
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-06-30 09:06:23 -0300

    teste cors

--- src/main/java/com/semanaArte/controller/VisitanteController.java
+++ src/main/java/com/semanaArte/controller/VisitanteController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import com.semanaArte.model.Visitante;
 import com.semanaArte.service.VisitanteServiceImpl;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/api/")
 @CrossOrigin(origins = "http://localhost:3000/", allowedHeaders = "*", maxAge = 3600)
@@ -16,6 +18,14 @@ public class VisitanteController {
 	
     @Autowired
     VisitanteServiceImpl visitanteService;
+
+    @RequestMapping(value= "/api/", method=RequestMethod.OPTIONS)
+    public void corsHeaders(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+        response.addHeader("Access-Control-Max-Age", "3600");
+    }
     
 	@GetMapping(value = "/", produces = "application/json")
 	public List<Visitante> findAll() {
