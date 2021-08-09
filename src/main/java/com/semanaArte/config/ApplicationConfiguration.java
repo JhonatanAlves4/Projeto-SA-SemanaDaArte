809f40136d46bb5a7f799ed65925be710a03e1ff
Jhonatan Alves Pereira <71884829+JhonatanAlves4@users.noreply.github.com>
2021-08-09 11:10:52 -0300

    Add files via upload

---
+++ src/main/java/com/semanaArte/config/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.semanaArte.config;
+
+import org.springframework.web.bind.annotation.*;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
