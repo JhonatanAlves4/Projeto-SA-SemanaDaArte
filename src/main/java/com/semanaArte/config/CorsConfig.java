20054ca54c9a49a5325992ebf1eea6e03c0b5203
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-08-09 19:26:47 -0300

    Liberando o CORS

---
+++ src/main/java/com/semanaArte/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.semanaArte.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
--- src/main/java/com/semanaArte/controller/VisitanteController.java
+++ src/main/java/com/semanaArte/controller/VisitanteController.java
@@ -8,23 +8,17 @@ import org.springframework.web.bind.annotation.*;
 import com.semanaArte.model.Visitante;
 import com.semanaArte.service.VisitanteServiceImpl;
 
+import javax.persistence.Access;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("api/")
 @CrossOrigin(origins = "https://sa-semana-da-arte-2021.vercel.app", allowedHeaders = "*", maxAge = 3600)
+
 public class VisitanteController {
 	
     @Autowired
      VisitanteServiceImpl visitanteService;
-
-    /*@RequestMapping(value= "api/", method=RequestMethod.OPTIONS)
-    public void corsHeaders(HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
-        response.addHeader("Access-Control-Max-Age", "3600");
-    }*/
     
 	@GetMapping(value = "/", produces = "application/json")
 	public List<Visitante> findAll() {
